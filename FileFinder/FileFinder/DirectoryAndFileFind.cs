db66e81b161648255b3262c1fa3e2ade211480fc
cibergod <cibergod1@gmail.com>
2015-07-15 13:19:16 +0300

    добавляем класс нахождения файлов и папок

---
+++ FileFinder/FileFinder/DirectoryAndFileFind.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileFinder
+{
+    class DirectoryAndFileFind
+    {
+        //структура описания входных параметров
+        struct inputinfo 
+        {
+            //от куда начинать поиск
+            public string StartPatchFilder;
+            //если ищем директорию то передаем ее название
+            public string SearchFolder;
+            //если ищем файл то передаем его название
+            public string SearchFile;
+        }
+    }
+}
---
+++ FileFinder/FileFinder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
