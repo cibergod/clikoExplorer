ccd6d556e081370c49fa7b9ea9e3dea1449559ee
cibergod <cibergod1@gmail.com>
2015-07-20 14:00:14 +0300

    Описание обновления и создания таблици Digest.xml

--- disquisition/DBMotion/CreateDataReader/CreateDataReader/DataImage.cs
+++ disquisition/DBMotion/CreateDataReader/CreateDataReader/DataImage.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 
+using System.Data;
+
 namespace CreateDataReader
 {
-    #region структура хранения описания столбцов таблици
 
-    #endregion 
+  
 
     //класс будет хранить структуры для описания таблиц представлений 
     //Когда сервис прочтет данные он оставит после себя несколько таблиц с описанием найденных данных
@@ -26,7 +27,46 @@ namespace CreateDataReader
     //класс описания таблицы со списков отчетностей
     class Digest
     {
-        
+       
+        DataTable 
+                  MyDigest = new DataTable("Digest");
+
+        string NameDigestXml = "Digest.xml";
+
+        #region описание столбцов таблици Digest
+        void digestInit()
+        {
+            DataColumn column = new DataColumn();
+            //уникальный номер справочника
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "ID";
+            MyDigest.Columns.Add(column);
+    
+            //имя справочника
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "Name";
+            MyDigest.Columns.Add(column);
+
+            //дата последнего обновления
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.DateTime");
+            column.ColumnName = "LastUpdate";
+            MyDigest.Columns.Add(column);
+
+            //путь к справочнику
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "patch";
+            MyDigest.Columns.Add(column);
+
+            //путь к справочнику
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "Kliko_date";
+            MyDigest.Columns.Add(column);
+        }
+        #endregion
 
         #region переменные для заполнения таблицы
         //параметры для записи в таблицу
@@ -69,7 +109,6 @@ namespace CreateDataReader
             //по требованию возвращаем имя отчетности
             get { return Name; }
             //получаем имя отчетности на основе пути к ней
-            set { Name = getDigestName(patch); }
         }
 
         //описываем поля справочника (путь к папке с конфигом)
@@ -83,18 +122,114 @@ namespace CreateDataReader
         public DateTime DateUpdate
         {
             get { return LastUpdate; }
-            set { LastUpdate = getDataUpdate(patch); }
         }
 
         //описываем поля справочника (путь к справочнику)
         public DateTime Klik_odate
         {
             get { return Klikodate; }
-            set { Klikodate = getDataKlico(patch); }
         }
     #endregion
 
 
+        //сохранение данных в таблице 
+        void SaveTable() 
+        {
+            //если есть что записать
+            if (MyDigest.Rows.Count > 0)
+            {
+                //в любом случае фиксим результат в виде файла
+                //сохраняем в xml фай таблицу
+                System.IO.StringWriter writer = new System.IO.StringWriter();
+                //создаем схему документа
+                MyDigest.WriteXml(writer, XmlWriteMode.WriteSchema, true);
+                //сохраняем данные в виде файла
+                File.WriteAllText(NameDigestXml, writer.ToString());
+            }
+        }
+
+        //добавление отчетности в таблицу если его там нет 
+        public void ADDRowDigest() 
+        {
+           
+            
+            //заполняем данные на основе пути к файлу отчетности
+            LastUpdate = getDataUpdate(patch);
+            Klikodate = getDataKlico(patch);
+            Name = getDigestName(patch); 
+            
+             //если файл существует пробуем его загрузить в виде таблицы
+            if (File.Exists(NameDigestXml))
+            {
+                //читаем данные из XML файла 
+                var stringReader = new StringReader(File.ReadAllText(NameDigestXml));
+                //загружаем их в виде таблици
+                MyDigest.ReadXml(stringReader);
+                //пробуем найти в таблице отчетность по такому же пути 
+                DataRow[] ResultSelect =  MyDigest.Select("patch = '" + patch+"'");
+                //проверяем нашли ли мы запись если нашли нужно проверить равна ли она тем параметрам которые мы задали 
+                if (ResultSelect.Length > 0)
+                {
+                    //нужно ли сохранять данные 
+                    bool TrySave = false;
+                    //хоть найденная запись и будет одна но все же 
+                    foreach (DataRow S in ResultSelect)
+                    {
+                        //есои нашли хоть одно различие в записях сохраняем данные 
+
+                        //если имя не совпадает обновляем его 
+                        if (S["Name"] != Name)
+                        {
+                            S["Name"] = Name;
+                            TrySave = true;
+                        }
+                        //если дата не совпадает обновляем ее
+                        if (S["LastUpdate"].ToString() != LastUpdate.ToString())
+                        {
+                            S["LastUpdate"] = LastUpdate;
+                            TrySave = true;
+                        }
+                        //если дата файла kliko не сопадает то обновляем ее 
+                        if (S["Kliko_date"].ToString() != Klik_odate.ToString())
+                        {
+                            S["Kliko_date"] = Klik_odate;
+                            TrySave = true;
+                        }
+
+
+                        if (TrySave)
+                        {
+
+                            SaveTable();
+                        }
+                    }
+                }
+                else 
+                {
+                    //добавляем данные в таблицу
+
+                }
+
+            }
+            else 
+            {
+                //создаем таблицу в памяти
+                digestInit();
+                //заполняем данными 
+                DataRow NewRow = MyDigest.NewRow();
+                //приставиваем значее будет считаться порядковым номером
+                NewRow["ID"] = 1;
+                NewRow["Name"]=Name;
+                NewRow["LastUpdate"] =LastUpdate;
+                NewRow["patch"] = patch;
+                NewRow["Kliko_date"] = Klik_odate;
+                MyDigest.Rows.Add(NewRow);
+                //сохранение данных в таблице 
+                SaveTable();
+            }
+
+         
+        }
 
     }
 
--- disquisition/DBMotion/CreateDataReader/CreateDataReader/Program.cs
+++ disquisition/DBMotion/CreateDataReader/CreateDataReader/Program.cs
@@ -11,8 +11,14 @@ namespace CreateDataReader
         static void Main(string[] args)
         {
 
+            Digest MyDigest = new Digest();
 
+            MyDigest.Digestpatch = @"d:\Cliko\DATA\CFG\KLIKOCFG.DB";
 
+            MyDigest.ADDRowDigest();
+
+            //
+            Console.WriteLine("готово");
             Console.ReadKey();
         }
     }
